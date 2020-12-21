ddb6fdd1203d271dec2879af9744728bc339f740
hobohuang <1536888730@qq.com>
2020-12-21 22:39:21 +0800

    初始化目录结构，格式化初始样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/css/clear.scss"//格式化全局初始样式
+import "./assets/css/index.scss"//全局样式文件
 
 createApp(App)
   .use(store)
